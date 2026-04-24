6df589ce61c78112160792ce569bf845df7b40c5
MetaDC <150433207+MetaDC@users.noreply.github.com>
2026-04-24 17:24:09 +0530

    delete .extra

--- extract.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-doc = fitz.open('Salon Frenyz(April).pdf')
-text = ""
-for page in doc:
-    text += page.get_text()
-
-with open('pdf_content_pymupdf.txt', 'w') as f:
-    f.write(text)
